80e84c10194b6903788274b0b96a9489890a3cb6
gs1068 <genta.otias@gmail.com>
2022-03-13 21:05:27 +0900

    [add]メモ追加

--- src/api/main.go
+++ src/api/main.go
@@ -11,11 +11,18 @@ import (
 )
 
 func main() {
+	// ①config.NewDB():dbを返している
+	// ②dbを含む構造体{Conn: db}をCRUDのインターフェイス型に変換している
 	taskRepository := infra.NewTaskRepository(config.NewDB())
+	// ③レポジトリのインターフェイスをusecaseのインターフェイスに代入している
 	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	// ④usecaseのインターフェイスをhandlerのインターフェイスに代入している
 	taskHandler := handler.NewTaskHandler(taskUsecase)
 
+	// 上記で、
+	// ①infrastructure → ②domain → ③usecase → ④interfaceを実現している
 	e := echo.New()
+	// ルーティングの定義
 	handler.InitRouting(e, taskHandler)
 	e.Logger.Fatal(e.Start(":8888"))
 }
--- src/infra/task.go
+++ src/infra/task.go
@@ -1,56 +1,56 @@
 package infra
 
 import (
-    "github.com/gs1068/golang_ddd_sample/domain/model"
-    "github.com/gs1068/golang_ddd_sample/domain/repository"
+	"github.com/gs1068/golang_ddd_sample/domain/model"
+	"github.com/gs1068/golang_ddd_sample/domain/repository"
 
-    "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 // TaskRepository task repositoryの構造体
 type TaskRepository struct {
-    Conn *gorm.DB
+	Conn *gorm.DB
 }
 
 // NewTaskRepository task repositoryのコンストラクタ
 func NewTaskRepository(conn *gorm.DB) repository.TaskRepository {
-    return &TaskRepository{Conn: conn}
+	return &TaskRepository{Conn: conn}
 }
 
 // Create taskの保存
 func (tr *TaskRepository) Create(task *model.Task) (*model.Task, error) {
-    if err := tr.Conn.Create(&task).Error; err != nil {
-        return nil, err
-    }
+	if err := tr.Conn.Create(&task).Error; err != nil {
+		return nil, err
+	}
 
-    return task, nil
+	return task, nil
 }
 
 // FindByID taskをIDで取得
 func (tr *TaskRepository) FindByID(id int) (*model.Task, error) {
-    task := &model.Task{ID: id}
+	task := &model.Task{ID: id}
 
-    if err := tr.Conn.First(&task).Error; err != nil {
-        return nil, err
-    }
+	if err := tr.Conn.First(&task).Error; err != nil {
+		return nil, err
+	}
 
-    return task, nil
+	return task, nil
 }
 
 // Update taskの更新
 func (tr *TaskRepository) Update(task *model.Task) (*model.Task, error) {
-    if err := tr.Conn.Model(&task).Update(&task).Error; err != nil {
-        return nil, err
-    }
+	if err := tr.Conn.Model(&task).Update(&task).Error; err != nil {
+		return nil, err
+	}
 
-    return task, nil
+	return task, nil
 }
 
 // Delete taskの削除
 func (tr *TaskRepository) Delete(task *model.Task) error {
-    if err := tr.Conn.Delete(&task).Error; err != nil {
-        return err
-    }
+	if err := tr.Conn.Delete(&task).Error; err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
