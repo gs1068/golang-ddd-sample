a58e8eef7342b7077ae339649b070e4d9e5100b9
gs1068 <genta.otias@gmail.com>
2022-03-19 21:21:24 +0900

    [update]Goルーチン追加

--- database/seeds/seed_files/timeline_seed.go
+++ database/seeds/seed_files/timeline_seed.go
@@ -3,6 +3,7 @@ package seed
 import (
 	"fmt"
 	"strconv"
+	"sync"
 
 	"github.com/jinzhu/gorm"
 )
@@ -13,7 +14,9 @@ type Timeline struct {
 	Content string
 }
 
-func TimelinesSeed(db *gorm.DB) error {
+func TimelinesSeed(db *gorm.DB, wg *sync.WaitGroup) error {
+	defer wg.Done()
+
 	for i := 0; i < 300; i++ {
 		contet := "みんなは生涯同時にこの発表者に従って訳の時に歩くんあり。ぼんやり時間で把持ごとは毫もこういう談判ですですだってに聴このにおきでをは創設進んたたいが、当然にも直さたずたです。" + strconv.Itoa(i)
 		timeline := Timeline{UserID: i + 1, Content: contet}
