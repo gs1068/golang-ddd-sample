85ca00a4c3b7433b9f29b49b20f6f7471fb04d77
gs1068 <genta.otias@gmail.com>
2022-03-24 15:59:02 +0900

    User CRUD test完了

--- mock/mock_repository_user_test.go
+++ mock/mock_repository_user_test.go
@@ -2,7 +2,6 @@ package mock_repository
 
 import (
 	"errors"
-	"fmt"
 	reflect "reflect"
 	"testing"
 
@@ -13,11 +12,16 @@ import (
 )
 
 var err error
+var errNotFound = errors.New("record not found")
+var errEmptyUserName = errors.New("ユーザーネームを入力してください")
+var expected = &model.User{
+	ID:       1,
+	UserName: "test",
+}
 
 func TestUserSuccessCreate(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-	var expected = &model.User{}
 	var args = &model.User{
 		UserName: "test",
 	}
@@ -26,11 +30,10 @@ func TestUserSuccessCreate(t *testing.T) {
 	mockUser.EXPECT().Create(args).Return(expected, err)
 	userUsecase := usecase.NewUserUsecase(mockUser)
 
-	result, err := userUsecase.Create("test")
-	if err != nil {
+	result, returnErr := userUsecase.Create("test")
+	if returnErr != nil {
 		t.Error("Actual Create() is not same as expected")
 	}
-
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Actual Create() is not same as expected")
 	}
@@ -38,16 +41,14 @@ func TestUserSuccessCreate(t *testing.T) {
 
 func TestUserFailureCreate(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
-	errExpected := errors.New("ユーザーネームを入力してください")
 	defer mockCtrl.Finish()
-
 	mockUser := NewMockUserRepository(mockCtrl)
 	userUsecase := usecase.NewUserUsecase(mockUser)
-	_, err := userUsecase.Create("")
-	if err == nil {
+	_, returnErr := userUsecase.Create("")
+	if returnErr == nil {
 		t.Error("Actual Create() is not same as expected")
 	}
-	assert.Equal(t, err, errExpected)
+	assert.Equal(t, returnErr, errEmptyUserName)
 }
 
 func TestUserFindAllSuccess(t *testing.T) {
@@ -57,8 +58,8 @@ func TestUserFindAllSuccess(t *testing.T) {
 	mockUser := NewMockUserRepository(mockCtrl)
 	mockUser.EXPECT().FindAll().Return(expected, err)
 	userUsecase := usecase.NewUserUsecase(mockUser)
-	result, err := userUsecase.FindAll()
-	if err != nil {
+	result, returnErr := userUsecase.FindAll()
+	if returnErr != nil {
 		t.Error("Actual Create() is not same as expected")
 	}
 
@@ -70,13 +71,12 @@ func TestUserFindAllSuccess(t *testing.T) {
 func TestUserDeleteSeccess(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-	var expected = &model.User{}
 	mockUser := NewMockUserRepository(mockCtrl)
-	mockUser.EXPECT().FindByID(gomock.Any()).Return(expected, err)
-	mockUser.EXPECT().Delete(gomock.Any()).Return(err)
+	mockUser.EXPECT().FindByID(gomock.Any()).Return(nil, err)
+	mockUser.EXPECT().Delete(gomock.Any()).Return(nil)
 	userUsecase := usecase.NewUserUsecase(mockUser)
-	err := userUsecase.Delete(1)
-	if err != nil {
+	returnErr := userUsecase.Delete(1)
+	if returnErr != nil {
 		t.Error("Actual Delete() is not same as expected")
 	}
 }
@@ -84,24 +84,96 @@ func TestUserDeleteSeccess(t *testing.T) {
 func TestUserDeleteFailure(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-	findID := 44444
-	// var args = &model.User{
-	// 	ID:       0,
-	// 	UserName: "",
-	// }
+	userID := 44444444
 	var expected = &model.User{}
 	mockUser := NewMockUserRepository(mockCtrl)
-	// mockUser.EXPECT().FindByID(findID).Return(expected, err)
-	// fmt.Print("エラー", err)
-	// mockUser.EXPECT().Delete(expected).Return(err)
+	mockUser.EXPECT().FindByID(userID).Return(expected, errNotFound)
 	userUsecase := usecase.NewUserUsecase(mockUser)
-
-	err := userUsecase.Delete(findID)
-
-	fmt.Print(expected)
-
-	fmt.Print("エラー", err)
-	if err != nil {
+	returnErr := userUsecase.Delete(userID)
+	if returnErr == nil {
 		t.Error("Actual Delete() is not same as expected")
+	}
+}
+
+func TestUserFIndByIDSuccess(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	var userID = 1
+	mockUser := NewMockUserRepository(mockCtrl)
+	mockUser.EXPECT().FindByID(userID).Return(expected, err)
+	userUsecase := usecase.NewUserUsecase(mockUser)
+	result, returnErr := userUsecase.FindByID(userID)
+	if returnErr != nil {
+		t.Error("Actual FindByID() is not same as expected")
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Actual FindByID() is not same as expected")
+	}
+}
+
+func TestUserFindByIDFailure(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	userID := 44444444
+	mockUser := NewMockUserRepository(mockCtrl)
+	mockUser.EXPECT().FindByID(userID).Return(nil, errNotFound)
+	userUsecase := usecase.NewUserUsecase(mockUser)
+	_, returnErr := userUsecase.FindByID(userID)
+	if returnErr == nil {
+		t.Error("Actual FindByID() is not same as expected")
+	}
+}
+
+func TestUserUpdateSuccess(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	var userID = 1
+	var args = &model.User{
+		ID:       userID,
+		UserName: "test",
+	}
+	mockUser := NewMockUserRepository(mockCtrl)
+	mockUser.EXPECT().FindByID(userID).Return(expected, err)
+	mockUser.EXPECT().Update(args).Return(expected, err)
+	userUsecase := usecase.NewUserUsecase(mockUser)
+	result, returnErr := userUsecase.Update(userID, "test")
+	if returnErr != nil {
+		t.Error("Actual Update() is not same as expected")
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Actual Create() is not same as expected")
+	}
+}
+
+func TestUserUpdateFailureNotFound(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	userID := 44444444
+	mockUser := NewMockUserRepository(mockCtrl)
+	mockUser.EXPECT().FindByID(userID).Return(nil, errNotFound)
+	userUsecase := usecase.NewUserUsecase(mockUser)
+	_, returnErr := userUsecase.Update(userID, "test")
+	if returnErr == nil {
+		t.Error("Actual Update() is not same as expected")
+	}
+	if !reflect.DeepEqual(errNotFound, returnErr) {
+		t.Errorf("Actual Create() is not same as expected")
+	}
+}
+
+func TestUserUpdateFailureEmptyUserName(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	var userID = 1
+	mockUser := NewMockUserRepository(mockCtrl)
+	mockUser.EXPECT().FindByID(userID).Return(expected, err)
+	userUsecase := usecase.NewUserUsecase(mockUser)
+	_, returnErr := userUsecase.Update(userID, "")
+	if returnErr == nil {
+		t.Error("Actual Update() is not same as expected")
+	}
+	if !reflect.DeepEqual(returnErr, errEmptyUserName) {
+		t.Errorf("Actual Create() is not same as expected")
 	}
 }
